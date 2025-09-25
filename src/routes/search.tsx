c7973422715c4eb9c06ada3a3bcf45dd6b5843fc
shrutikapoor08 <shruti.kapoor08@gmail.com>
2025-09-24 18:12:49 -0700

    Using loader to fix issues

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -1,25 +1,47 @@
-import { createFileRoute } from '@tanstack/react-router'
-// ...existing code...
-import { useQuery } from '@tanstack/react-query'
-import type { UseQueryOptions } from '@tanstack/react-query'
+import { createFileRoute, useLoaderData } from '@tanstack/react-router'
 import MovieCard from '../components/MovieCard'
 import useSearchStore from '@/store/searchStore'
-import useBaseMovieStore from '@/store/moviesStore'
 import performSearch from '@/lib/performSearch'
+
+import type { TMDBResponse, Movie } from "@/types";
+
+
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+
 export const Route = createFileRoute('/search')({
     component: SearchComponent,
+    loader: async () => {
+        const token = import.meta.env.VITE_TMDB_AUTH_TOKEN;
+        const response = await fetch(
+            API_URL,
+            {
+                headers: {
+                    'accept': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                },
+            }
+        );
+        if (!response.ok) {
+            throw new Error(`Failed to fetch movies: ${response.statusText}`);
+        }
+        const data = (await response.json()) as TMDBResponse;
+        const movies = data.results;
+        return {
+            baseMovies: movies
+        }
+    }
 })
 
 function SearchComponent() {
     const search = Route.useSearch() as Record<string, string | undefined>;
+    const { baseMovies } = Route.useLoaderData();
     const movie = search.movie ?? '';
 
-    const results = useSearchStore(state => state.results);
-
+    const results = performSearch(movie ?? '', baseMovies).data;
 
     return (
         <div className='container relative mx-auto mt-6 max-w-6xl px-6'>
-            <h2 className='text-2xl font-semibold mt-6'>Search Results for "{movie}"</h2>
+            {movie && <h2 className='text-2xl font-semibold mt-6'>Search Results for "{movie}"</h2>}
 
             <div aria-live="polite" aria-atomic="true" className="sr-only">
                 {movie ? `${results.length} results for ${movie}` : `${results.length} results`}
